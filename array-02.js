39f56a002f2b065fd9fd19280ad3487ebc512b92
Azizu Haque <azizul.contact.me@gmail.com>
2024-05-04 12:40:06 +0300

    learning object

--- array-02.js
+++ array-02.js
@@ -18,9 +18,9 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 const real_another_array = another_array.flat(Infinity);
 console.log(real_another_array);
 
-console.log(Array.isArray("Hitesh"));
-console.log(Array.from("Hitesh"));
-console.log(Array.from({ name: "hitesh" })); // interesting
+console.log(Array.isArray("Azizul"));
+console.log(Array.from("Azizul"));
+console.log(Array.from({ name: "Azizul" })); // interesting
 
 let score1 = 100;
 let score2 = 200;
---
+++ object-01.js
@@ -0,0 +1,35 @@
+// singleton
+// Object.create
+
+// object literals
+
+const JsUser = {
+  name: "Hitesh",
+  "full name": "Hitesh Choudhary",
+  [mySym]: "mykey1",
+  age: 18,
+  location: "Jaipur",
+  email: "hitesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"]
+};
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com";
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS user");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS user, ${this.name}`);
+};
+
+JsUser.greetingTwo();
+JsUser.greeting();
