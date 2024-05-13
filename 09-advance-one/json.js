86941befcbc1a7350c98a81afc236a95acf74299
user-azizul <azizul.contact.me@gmail.com>
2024-05-13 15:53:28 +0300

    json.js

---
+++ 09-advance-one/json.js
@@ -0,0 +1,8 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
